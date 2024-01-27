bea23c407b09118b46b8c4e7d9aa02e3c62cd8ad
Alexander Algasov <wisestump@yandex.ru>
2024-01-27 09:00:06 +0200

    Friction = 0

---
+++ Assets/InputActions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class InputActions : MonoBehaviour
+{
+    public int Move { get; private set; }
+
+    public bool IsJumpActive { get; private set; }
+
+    private void Update()
+    {
+        // Movement controls
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            Move = Input.GetKey(KeyCode.A) ? -1 : 1;
+        }
+        else
+        {
+            Move = 0;
+        }
+         
+        IsJumpActive = Input.GetKeyDown(KeyCode.Space);
+    }
+}
