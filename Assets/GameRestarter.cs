23e261bf17b1dca53e37ed98223108f4423a8347
Alexander Algasov <wisestump@yandex.ru>
2024-01-27 14:27:34 +0200

    Add restarter

---
+++ Assets/GameRestarter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+class GameRestarter : MonoBehaviour
+{
+    public InputActions InputActions;
+    public ChoiceAction Action;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    private void Update()
+    {
+        if (InputActions.IsRestartActive == false)
+            return;
+
+        Action.Perform(Player.Instance);
+    }
+}
