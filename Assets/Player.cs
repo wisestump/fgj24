3461fd41837c6b58719687c8a7afb3893c88214a
Alexander Algasov <wisestump@yandex.ru>
2024-01-27 13:45:39 +0200

    Implement teleport choice

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,116 +1,12 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-//[RequireComponent(typeof(CapsuleCollider2D))]
 public class Player : MonoBehaviour
 {
-    // Move player in 2D space
-    public float maxSpeed = 3.4f;
-    public float jumpHeight = 6.5f;
-    public float gravityScale = 1.5f;
-    public Camera mainCamera;
-    public InputActions InputActions;
-
-    bool facingRight = true;
-    float moveDirection = 0;
-    bool isGrounded = false;
-    Rigidbody2D r2d;
-    BoxCollider2D mainCollider;
-    Transform t;
-
     public static Player Instance { get; private set; }
 
     private void Awake()
     {
         Debug.Assert(Instance == null);
         Instance = this;
-    }
-
-    // Use this for initialization
-    void Start()
-    {
-        t = transform;
-        r2d = GetComponent<Rigidbody2D>();
-        mainCollider = GetComponent<BoxCollider2D>();
-        r2d.freezeRotation = true;
-        r2d.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
-        r2d.gravityScale = gravityScale;
-        facingRight = t.localScale.x > 0;
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // Movement controls
-        if (InputActions.Move != 0 && (isGrounded || Mathf.Abs(r2d.velocity.x) > 0.01f))
-        {
-            moveDirection = InputActions.Move;
-        }
-        else
-        {
-            if (isGrounded || r2d.velocity.magnitude < 0.01f)
-            {
-                moveDirection = 0;
-            }
-        }
-
-        // Change facing direction
-        if (moveDirection != 0)
-        {
-            if (moveDirection > 0 && !facingRight)
-            {
-                facingRight = true;
-                t.localScale = new Vector3(Mathf.Abs(t.localScale.x), t.localScale.y, transform.localScale.z);
-            }
-            if (moveDirection < 0 && facingRight)
-            {
-                facingRight = false;
-                t.localScale = new Vector3(-Mathf.Abs(t.localScale.x), t.localScale.y, t.localScale.z);
-            }
-        }
-
-        // Jumping
-        if (InputActions.IsJumpActive && isGrounded)
-        {
-            r2d.velocity = new Vector2(r2d.velocity.x, jumpHeight);
-        }
-    }
-
-    void FixedUpdate()
-    {
-        Bounds colliderBounds = mainCollider.bounds;
-        float colliderRadius = mainCollider.size.x * 0.4f * Mathf.Abs(transform.localScale.x);
-        Vector3 groundCheckPos = colliderBounds.min + new Vector3(colliderBounds.size.x * 0.5f, colliderRadius * 0.9f, 0);
-        // Check if player is grounded
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheckPos, colliderRadius);
-        //Check if any of the overlapping colliders are not player collider, if so, set isGrounded to true
-        isGrounded = DoesIntersect(colliders);
-
-        Vector3 rightWallCheck = colliderBounds.max + new Vector3(colliderBounds.size.x * 0.5f, colliderRadius * 0.9f, 0);
-        var touchingRightWall = DoesIntersect(Physics2D.OverlapCircleAll(rightWallCheck, colliderRadius));
-
-        // Apply movement velocity
-        r2d.velocity = new Vector2((moveDirection) * maxSpeed, r2d.velocity.y);
-
-        // Simple debug
-        Debug.DrawLine(groundCheckPos, groundCheckPos - new Vector3(0, colliderRadius, 0), isGrounded ? Color.green : Color.red);
-        Debug.DrawLine(groundCheckPos, groundCheckPos - new Vector3(colliderRadius, 0, 0), isGrounded ? Color.green : Color.red);
-    }
-
-    private bool DoesIntersect(Collider2D[] colliders)
-    {
-        if (colliders.Length > 0)
-        {
-            for (int i = 0; i < colliders.Length; i++)
-            {
-                if (colliders[i] != mainCollider)
-                {
-                    return true;
-                }
-            }
-        }
-
-        return false;
     }
 }
