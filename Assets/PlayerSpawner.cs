36f805890bd36b6d020730837379216c96882d60
Alexander Algasov <wisestump@yandex.ru>
2024-01-27 23:28:05 +0200

    Map adjustments

---
+++ Assets/PlayerSpawner.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+class PlayerSpawner : MonoBehaviour
+{
+    public GameObject[] Skins;
+
+    private void Start()
+    {
+        Instantiate(Skins[Random.Range(0, Skins.Length)]);
+    }
+}
