b8fb8a34ed1925362a6ee961b5edf4175d0dc4f9
Alexander Algasov <wisestump@yandex.ru>
2024-01-27 15:48:35 +0200

    Add bounds to panel

--- Assets/Panel.cs
+++ Assets/Panel.cs
@@ -4,8 +4,11 @@ class Panel : MonoBehaviour
 {
     [SerializeField]
     private Transform playerStartingPoint;
+    [SerializeField]
+    private BoxCollider2D boxCollider2D;
 
     public Vector2 Size => transform.localScale;
+    public Bounds Bounds => boxCollider2D.bounds;
 
     public Vector3 PlayerStartingPoint => playerStartingPoint.position;
 }
