bb5240f48a1e7e2c7a664118f1cdbf6de2864dcc
Alexander Algasov <wisestump@yandex.ru>
2024-01-28 16:34:46 +0200

    Disable claw earlier

--- Assets/CutsceneChoiceAction.cs
+++ Assets/CutsceneChoiceAction.cs
@@ -12,10 +12,10 @@ class CutsceneChoiceAction : ChoiceAction
     {
         player.HasWon = true;
         player.SetPhysicsEnabled(false);
+        Claw.Instance.gameObject.SetActive(false);
         CameraFadeout.Instance.InstantBlackScreen();
         yield return new WaitForSeconds(1f);
         CameraFadeout.Instance.FadeOutOfBlack();
         Endscreen.Instance.Show();
-        Claw.Instance.gameObject.SetActive(false);
     }
 }
