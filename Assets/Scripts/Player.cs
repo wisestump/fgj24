85348d1e88f1eb31b7936b55c64a4069ad6b8087
Ted <tedzolotarev@gmail.com>
2024-01-28 12:39:47 +0200

    Props

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,13 @@ class Player : MonoBehaviour
     public Panel CurrentPanel { get; private set; }
     public Movement Movement { get; private set; }
 
+    public JetpackProp Jetpack;
+    public GameObject SpaceHelmet;
+    public GameObject DivingMask;
+    public GameObject FoamFinger;
+    public GameObject Swatter;
+    public GameObject FartCloud;
+
     private void Awake()
     {
         Debug.Assert(Instance == null);
@@ -20,6 +27,8 @@ class Player : MonoBehaviour
     private void Start()
     {
         CameraFollower.Instance.Player = gameObject;
+        // SetDivingMaskActive(true);
+        // SetFartCloudActive(true);
     }
 
     public void SetPanel(Panel panel)
@@ -31,7 +40,50 @@ class Player : MonoBehaviour
     public void EnableJetpack()
     {
         Movement.EnableJetpack();
+        Jetpack.gameObject.SetActive(true);
+
     }
+
+    public void DisableJetpack()
+    {
+        Movement.DisableJetpack();
+        Jetpack.gameObject.SetActive(false);
+
+    }
+
+    public void SetSpaceHelmetActive(bool value) => SpaceHelmet.SetActive(value);
+    public void SetDivingMaskActive(bool value) => DivingMask.SetActive(value);
+    public void SetFoamFingerActive(bool value) => FoamFinger.SetActive(value);
+    public void SetSwatterActive(bool value) => Swatter.SetActive(value);
+    public void SetFartCloudActive(bool value) => FartCloud.SetActive(value);
+    
+    public void DisableAllProps()
+    {
+        DisableJetpack();
+        SetSpaceHelmetActive(false);
+        SetDivingMaskActive(false);
+        SetFoamFingerActive(false);
+        SetSwatterActive(false);
+        SetFartCloudActive(false);
+    }
+
+    public bool Has1UP()
+    {
+        return FartCloud.activeSelf || FoamFinger.activeSelf || Swatter.activeSelf;
+    }
+
+    public bool Use1UP()
+    {
+        if(Has1UP())
+        {
+            SetFoamFingerActive(false);
+            SetSwatterActive(false);
+            SetFartCloudActive(false);
+            return true;
+        }
+        return false;
+    }
+
     public void SetPhysicsEnabled(bool value)
     {
         Movement.enabled = value;
